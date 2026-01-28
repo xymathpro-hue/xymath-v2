2230a4aa4cf07475acbc3bd5b9cb2e93cdb1e014
xymathpro-hue <xymathpro@gmail.com>
2026-01-28 08:42:33 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>XYMath - Plataforma Educacional</h1>
+      <p>Sistema em construção. Admin: xymathpro@gmail.com</p>
+    </div>
+  );
+}
